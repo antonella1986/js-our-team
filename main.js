48250302709bf6eb419e186727c3e5a698e10381
antonella1986 <antonella.leo.mail@gmail.com>
2025-02-24 16:57:39 +0100

    add imgs + edit style

--- main.js
+++ main.js
@@ -4,37 +4,37 @@ const teamMembers = [
       name: "Marco Bianchi",
       role: "Designer",
       email: "marcobianchi@team.com",
-      img: "img/male1.png"
+      img: "assets/img/male1.png"
     },
     {
       name: "Laura Rossi",
       role: "Front-end Developer",
       email: "laurarossi@team.com",
-      img: "img/female1.png"
+      img: "assets/img/female1.png"
     },
     {
       name: "Giorgio Verdi",
       role: "Back-end Developer",
       email: "giorgioverdi@team.com",
-      img: "img/male2.png"
+      img: "assets/img/male2.png"
     },
     {
       name: "Marta Ipsum",
       role: "SEO Specialist",
       email: "martarossi@team.com",
-      img: "img/female2.png"
+      img: "assets/img/female2.png"
     },
     {
       name: "Roberto Lorem",
       role: "SEO Specialist",
       email: "robertolorem@team.com",
-      img: "img/male3.png"
+      img: "assets/img/male3.png"
     },
     {
       name: "Daniela Amet",
       role: "Analyst",
       email: "danielaamet@team.com",
-      img: "img/female3.png"
+      img: "assets/img/female3.png"
     }
   ];
 
@@ -58,16 +58,18 @@ renderTeamMembers(teamMembers, cardsEl)
 
 function getMemberMarkup(memberObj) {
     // destrutturazione dell'oggetto
-    const {name, role, email, image} = memberObj
-    console.log(name.toUpperCase(), role, email, image);
+    const {name, role, email, img} = memberObj
+    console.log(name.toUpperCase(), role, email, img);
     
     const markup = `
         <div class="card col-4">
-            <img src="${image}" style="width: 100px;" alt="" class="card-mg-top">
-            <div class="card-body bg-dark text-light">
-                <h3><strong>"${name}"</strong></h3>
-                <p>"${role}"</p>
-                <p class="text-primary">"${email}"</p>
+            <div class="card-body d-flex bg-dark text-light text-start p-0">
+                <img src=${img} style="width: 30%;" alt="" class="card-mg-top me-3">
+                <div class="mt-2">
+                    <h3><strong>${name}</strong></h3>
+                    <p>${role}</p>
+                    <p class="text-primary">${email}</p>
+                </div>
             </div>
         </div>`
 
