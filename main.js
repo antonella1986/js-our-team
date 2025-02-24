72a311cb015b781dad8f7d7b43873621c709591a
antonella1986 <antonella.leo.mail@gmail.com>
2025-02-24 15:58:08 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,109 @@
+const formEl = document.getElementById("member_form")
+const cardsEl = document.querySelector(".cards")
+const teamMembers = [
+    {
+      name: "Marco Bianchi",
+      role: "Designer",
+      email: "marcobianchi@team.com",
+      img: "img/male1.png"
+    },
+    {
+      name: "Laura Rossi",
+      role: "Front-end Developer",
+      email: "laurarossi@team.com",
+      img: "img/female1.png"
+    },
+    {
+      name: "Giorgio Verdi",
+      role: "Back-end Developer",
+      email: "giorgioverdi@team.com",
+      img: "img/male2.png"
+    },
+    {
+      name: "Marta Ipsum",
+      role: "SEO Specialist",
+      email: "martarossi@team.com",
+      img: "img/female2.png"
+    },
+    {
+      name: "Roberto Lorem",
+      role: "SEO Specialist",
+      email: "robertolorem@team.com",
+      img: "img/male3.png"
+    },
+    {
+      name: "Daniela Amet",
+      role: "Analyst",
+      email: "danielaamet@team.com",
+      img: "img/female3.png"
+    }
+  ];
+
+console.log(teamMembers);
+
+
+function renderTeamMembers(membersArray, domEl) {
+    for (let i = 0; i < membersArray.length; i++) {
+        const thisMember = membersArray[i];
+        console.log(thisMember);
+        
+        // funzione che restituisce il markup
+        const markup = getMemberMarkup(thisMember)
+        console.log(markup);
+        
+        domEl.innerHTML += markup
+    }
+}
+
+renderTeamMembers(teamMembers, cardsEl)
+
+
+formEl.addEventListener("submit", (e) => {
+    
+    e.preventDefault()
+    
+    const name = document.querySelector('input[name="name"]').value
+    console.log(name);
+    const role = document.querySelector('input[name="role"]').value
+    console.log(role);
+    const email = document.querySelector('input[name="email"]').value
+    console.log(email);
+    const image = document.querySelector('input[name="image"]').value
+    console.log(image);
+
+    const member = {
+        name,
+        role,
+        email,
+        image
+    }
+    console.log(member);
+
+    // funzione che ritorna il markup
+    const markup = getMemberMarkup(member)
+    
+    cardsEl.innerText += markup // stampo la card dell'HTML e anche le card con i valori generati da ciò che inserisce l'utente negli input
+    
+    // versione estesa
+    // cardsEl.innerText = cardsEl.innerText + markup
+    
+});
+
+function getMemberMarkup(memberObj) {
+    // destrutturazione dell'oggetto
+    const {name, role, email, image} = memberObj
+    console.log(name, role, email, image);
+    
+    const markup = `
+        <div class="card col-4">
+            <img src="${image}" style="width: 232px;" alt="" class="card-mg-top">
+            <div class="card-body">
+                <h3>"${name}"</h3>
+                <p>"${role}"</p>
+                <p>"${email}"</p>
+            </div>
+        </div>`
+
+    console.log(markup);
+    return markup
+}
